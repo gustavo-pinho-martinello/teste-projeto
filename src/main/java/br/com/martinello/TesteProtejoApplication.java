7767f35a7a74b5c058fefb312386234664426ef3
kevin.carvalho <kevin.carvalho@TI21.martinello.localdomain>
2022-06-17 14:50:49 -0400

    .

---
+++ src/main/java/br/com/martinello/TesteProtejoApplication.java
@@ -0,0 +1,13 @@
+package br.com.martinello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteProtejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteProtejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/martinello/TesteProtejoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.martinello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteProtejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
